a23328a54e15a10b969597f95d113b788a6ef31d
taher.bensassi <taher.bensassi@brettingham.de>
2020-10-30 19:37:12 +0100

    [FEATURE] | Java | Create first crud Category test Empty

---
+++ src/test/java/com/ewd/report/controller/CategoryControllerTest.java
@@ -0,0 +1,28 @@
+package com.ewd.report.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @Test
+    void getAllCategories() {
+    }
+
+    @Test
+    void addCategory() {
+    }
+
+    @Test
+    void getCategoryById() {
+    }
+
+    @Test
+    void updateCategory() {
+    }
+
+    @Test
+    void deleteCategory() {
+    }
+}
